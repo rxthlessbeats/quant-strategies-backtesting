32893a25b49ef57a2c4f7cc67bcae05a52842871
Magnus <lutinyu@gmail.com>
2026-02-22 03:05:43 -0800

    install pre-commit

--- src/trading_rookie/data/get_data.py
+++ src/trading_rookie/data/get_data.py
@@ -4,27 +4,28 @@ import pandas as pd
 import time
 from datetime import datetime
 
-class DataDownloader():
+
+class DataDownloader:
     def __init__(self):
         self.headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
         }
 
     def _timestamp(self, date: str):
         """
         Convert date into Unix timestamp.
         """
-        return int(time.mktime(datetime.strptime(date, '%Y-%m-%d').timetuple()))
+        return int(time.mktime(datetime.strptime(date, "%Y-%m-%d").timetuple()))
 
     def stooq(self, symbol: str, start_date: str, end_date: str):
         """
         Download historical stock data from Stooq, fixed interval is 1d.
         """
-        
+
         data = web.DataReader(symbol, "stooq", start_date, end_date)
         return data
 
-    def yahoo(self, symbol: str, start_date: str, end_date: str, interval: str='1d'):
+    def yahoo(self, symbol: str, start_date: str, end_date: str, interval: str = "1d"):
         """
         Download historical stock data from Yahoo Finance, minimum interval is 1m (only 8 days period can be downloaded).
         Recommended interval is 1d, 1h, 15m, 5m, 1m.
@@ -33,46 +34,53 @@ class DataDownloader():
         start_timestamp = self._timestamp(start_date)
         end_timestamp = self._timestamp(end_date)
 
-        url = f'https://query1.finance.yahoo.com/v8/finance/chart/{symbol}?period1={start_timestamp}&period2={end_timestamp}&interval={interval}'
+        url = f"https://query1.finance.yahoo.com/v8/finance/chart/{symbol}?period1={start_timestamp}&period2={end_timestamp}&interval={interval}"
         response = requests.get(url, headers=self.headers)
 
-        if response.json()['chart']['error'] is not None:
-            raise Exception(f'Failed to get stock data: Error code {response.status_code}: {response.json()["chart"]["error"]["description"]}')
+        if response.json()["chart"]["error"] is not None:
+            raise Exception(
+                f'Failed to get stock data: Error code {response.status_code}: {response.json()["chart"]["error"]["description"]}'
+            )
 
-        data = response.json()['chart']['result'][0]
-        quote = data['indicators']['quote'][0]
-        date = pd.to_datetime(data['timestamp'], unit='s')
+        data = response.json()["chart"]["result"][0]
+        quote = data["indicators"]["quote"][0]
+        date = pd.to_datetime(data["timestamp"], unit="s")
 
         # if interval is 1d
-        if 'd' in interval:
+        if "d" in interval:
             date = date.normalize()
-            adj_close = data['indicators']['adjclose'][0]
+            adj_close = data["indicators"]["adjclose"][0]
 
-            stock_data = pd.DataFrame({
-                'Date': date,
-                'Open': quote['open'],
-                'High': quote['high'],
-                'Low': quote['low'],
-                'Close': quote['close'],
-                'Volume': quote['volume'],
-                'Adj_close': adj_close['adjclose']
-            })
-        
+            stock_data = pd.DataFrame(
+                {
+                    "Date": date,
+                    "Open": quote["open"],
+                    "High": quote["high"],
+                    "Low": quote["low"],
+                    "Close": quote["close"],
+                    "Volume": quote["volume"],
+                    "Adj_close": adj_close["adjclose"],
+                }
+            )
+
         # if interval is 1h, 1m, 15m, 5m
-        elif 'h' in interval or 'm' in interval:
-            stock_data = pd.DataFrame({
-                'Date': date,
-                'Open': quote['open'],
-                'High': quote['high'],
-                'Low': quote['low'],
-                'Close': quote['close'],
-                'Volume': quote['volume'],
-            })
-            
-        stock_data.set_index('Date', inplace=True)
-        
+        elif "h" in interval or "m" in interval:
+            stock_data = pd.DataFrame(
+                {
+                    "Date": date,
+                    "Open": quote["open"],
+                    "High": quote["high"],
+                    "Low": quote["low"],
+                    "Close": quote["close"],
+                    "Volume": quote["volume"],
+                }
+            )
+
+        stock_data.set_index("Date", inplace=True)
+
         return stock_data
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     data_downloader = DataDownloader()
-    print(data_downloader.yahoo('AAPL', '2025-12-22', '2026-01-01', '1d'))
+    print(data_downloader.yahoo("AAPL", "2025-12-22", "2026-01-01", "1d"))
